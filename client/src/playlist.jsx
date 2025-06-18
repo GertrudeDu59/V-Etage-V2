9e48a5cc295334f0443203b78eb328ebc4d3e8f1
Ilyesse <ilyesse.jv2@outlook.fr>
2025-06-18 08:58:11 +0200

    Ajout de socket io

--- client/src/playlist.jsx
+++ client/src/playlist.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 import './sass/playlist.scss'
-
+// Ip de gev
 const socket = io('http://localhost:3001');
 
 const Playlist = () => {
